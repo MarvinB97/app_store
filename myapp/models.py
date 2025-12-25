d584fd87e119df30399d3a80bb44b22eb8e15800
MarvinB97 <marvinbryan16@gmail.com>
2025-12-25 16:03:31 -0500

    models 25-12-25

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    cantidad = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete= models.CASCADE)
