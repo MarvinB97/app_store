f749285dc8171697789a2f730a04beb729fa7459
MarvinB97 <marvinbryan16@gmail.com>
2025-12-27 20:19:50 -0500

    update urls 27-12-25

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello World")
+
+def index(request):
+    return HttpResponse('<h2>Index page</h2>')
+
+def hello(request, username):
+    return HttpResponse("Hello %s" % username)
 
 def about(request):
     return HttpResponse("<h1>About</h1>")
